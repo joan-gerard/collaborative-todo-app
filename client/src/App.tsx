7e943088535f0f4b2f47d8a863511d773b448ab3
Joan Gerard <joan.gerard@outlook.com>
2022-06-21 16:54:20 +0200

    add assets & set new ApolloClient

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        lists: {
+          merge(_existing, incoming) {
+            return incoming;
+          },
+        },
+        todos: {
+          merge(_existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache,
+});
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">Hello World</div>
+      </ApolloProvider>
+    </>
   );
 }
 
